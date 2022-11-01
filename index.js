efa39339c790da32d878fa57ee0e56212cecd9bb
wrunika <2805nika@gmail.com>
2022-11-01 13:31:15 +0300

    feat: create variables into js file

---
+++ index.js
@@ -0,0 +1,6 @@
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const container = document.querySelector('.container');
+const slidesCount = mainSlide.querySelectorAll('div').length;
